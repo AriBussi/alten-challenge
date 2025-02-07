9cfa818142c88d23fd617c1112d5b38a8d4d9373
AriBu__ <56850922+AriBussi@users.noreply.github.com>
2025-02-07 16:07:56 +0100

    temporary fix for duplicated items on backend

--- src/views/ListView.jsx
+++ src/views/ListView.jsx
@@ -6,8 +6,21 @@ const ListView = () => {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
+  // Backend is returning a duplicated item XMI-RN13P5G which causes unexpected behaviors
+  // A proper fix should be handled on the backend to ensure unique items in the response
+  // As a temporary workaround, I fetch more items and filter them down to get 20 unique ones
+  const getUniqueItems = (items, limit) => {
+    const uniqueItems = Array.from(
+      new Map(items.map((item) => [item.id, item])).values()
+    );
+
+    return uniqueItems.slice(0, limit);
+  };
+
   const fetchItems = async (searchString) => {
-    const query = searchString ? `search=${searchString}` : "limit=20";
+    if (!searchString && items.length === 20) return;
+
+    const query = searchString ? `search=${searchString}` : "limit=25";
 
     try {
       const res = await fetch(`/api/products?${query}`, {
@@ -19,7 +32,7 @@ const ListView = () => {
 
       const items = await res.json();
 
-      setItems(items);
+      setItems(getUniqueItems(items, 20));
     } catch (err) {
       console.error("Error fetching items", err);
     } finally {
