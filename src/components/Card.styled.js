8901f376552f84ade21c603de309bfe6ed69f6f6
AriBu__ <56850922+AriBussi@users.noreply.github.com>
2025-02-09 23:22:05 +0100

    card hover effect

--- src/components/Card.styled.js
+++ src/components/Card.styled.js
@@ -1,6 +1,25 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
+export const StyledImage = styled.img`
+  max-width: 100%;
+  max-height: 26rem;
+  place-self: center;
+`;
+
+export const StyledBrand = styled.p`
+  color: ${({ theme }) => theme.colors.neutral4};
+  font-size: 1rem;
+  transition: color 0.4s;
+`;
+
+export const StyledInfo = styled.dl`
+  color: ${({ theme }) => theme.colors.black};
+  display: flex;
+  justify-content: space-between;
+  transition: color 0.4s;
+`;
+
 export const StyledCard = styled(Link)`
   border: 0.5px solid ${({ theme }) => theme.colors.black};
   padding: 1.6rem;
@@ -11,6 +30,14 @@ export const StyledCard = styled(Link)`
   grid-gap: 1rem;
   width: 100%;
   min-height: 34.5rem;
+  background-image: linear-gradient(
+    ${({ theme }) => theme.colors.black},
+    ${({ theme }) => theme.colors.black}
+  );
+  background-size: 100% 0;
+  background-repeat: no-repeat;
+  background-position: bottom;
+  transition: background-size 0.4s;
 
   @media (min-width: 600px) {
     width: 50%;
@@ -18,26 +45,23 @@ export const StyledCard = styled(Link)`
 
   @media (min-width: 978px) {
     width: 33.3%;
+
+    &:hover {
+      background-size: 100% 100%;
+
+      ${StyledBrand} {
+        color: ${({ theme }) => theme.colors.neutral2};
+        transition: color 0.4s;
+      }
+
+      ${StyledInfo} {
+        color: ${({ theme }) => theme.colors.white};
+        transition: color 0.4s;
+      }
+    }
   }
 
   @media (min-width: 1200px) {
     width: 25%;
   }
-`;
-
-export const StyledImage = styled.img`
-  max-width: 100%;
-  max-height: 26rem;
-  place-self: center;
-`;
-
-export const StyledBrand = styled.p`
-  color: ${({ theme }) => theme.colors.neutral4};
-  font-size: 1rem;
-`;
-
-export const StyledInfo = styled.dl`
-  color: ${({ theme }) => theme.colors.black};
-  display: flex;
-  justify-content: space-between;
 `;
